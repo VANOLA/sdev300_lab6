7d3854f9c131f4e97811f62ae8b93caed100abb0
VANOLA <62918869+VANOLA@users.noreply.github.com>
2021-11-22 08:37:25 -0600

    Added date-time function and recipes

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,23 +1,29 @@
+"""
+lab6.py
+By: Vanessa Arreola
+November 23, 2021
+SDEV 300
+"""
+from datetime import datetime
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# main_recipes = ('Fricasé de Pollo (Chicken Fricaseé)', 
-#                'Ajiaco Criolla con Casabe (Creole Stew and Yucca Flatbread)',
-#                'Quimbombó con Platano (Okra Stew with Plantain Dumplings)')
-
-# side_recipes = ('Moros y Cristianos (Black Beans and Rice)',
-#                'Yucca con Mojo (Yucca with Creole Garlic Sauce)',
-#                'Platanós Maduros (Ripe Plantains')
-
-@app.route("/")
+@app.route("/")  # Homepage route
 @app.route("/home")
 def home():
-    return render_template('home.html', title='Cuban Cuisine', heading='Cuban Cuisine')
+    """Pass the date/time, title, and heading as arguments to the html template"""
+    current_time = datetime.now()  # current date and time
+    date_time = current_time.strftime("%d/%m/%Y, %H:%M:%S")  # create date/time to string
+    return render_template('home.html', date_time=date_time, title='Cuban Cuisine',
+    heading='Cuban Cuisine')
 
-@app.route("/maindishes")
+@app.route("/maindishes")  # Main dishes route
 def mains():
+    """Pass the title and heading as arguments to the html template"""
     return render_template('maindishes.html', title="Main Dishes", heading="Main Dishes")
 
-@app.route("/sides")
+@app.route("/sides")  # Side dishes route
 def sides():
+    """Pass the title and heading as arguments to the html template"""
     return render_template('sides.html', title="Side Dishes", heading="Side Dishes")
